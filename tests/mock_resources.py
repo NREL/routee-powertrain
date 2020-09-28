6674571bedb127cbd88cea21989400ab6669de9f
Jake Holden <jacob.holden@nrel.gov>
2020-09-28 12:43:48 -0600

    Update paths in simple prediction test

--- tests/mock_resources.py
+++ tests/mock_resources.py
@@ -6,7 +6,7 @@ from routee import read_model
 
 
 def mock_route() -> pd.DataFrame:
-    route_path = os.path.join("data", "trip_11394_1_metropia_austin_v2.csv")
+    route_path = os.path.join("routee-powertrain-test-data", "trip_11394_1_metropia_austin_v2.csv")
     route_df = pd.read_csv(route_path)
     route_df = route_df.rename(columns={
         'mean_mph': 'gpsspeed',
@@ -19,11 +19,11 @@ def mock_model(
         estimator: str,
 ):
     if estimator == "ExplicitBin":
-        model_path = os.path.join("data", "2015_Honda_Accord_HEV_Explicit_Bin.pickle")
+        model_path = os.path.join("..","powertrain","trained_models","standard", "2015_Honda_Accord_HEV_Explicit_Bin.pickle")
     elif estimator == "Linear":
-        model_path = os.path.join("data", "2015_Honda_Accord_HEV_Linear.pickle")
+        model_path = os.path.join("..","powertrain","trained_models","standard", "2015_Honda_Accord_HEV_Linear.pickle")
     elif estimator == "RandomForest":
-        model_path = os.path.join("data", "2015_Honda_Accord_HEV_Random_Forest.pickle")
+        model_path = os.path.join("..","powertrain","trained_models","standard", "2015_Honda_Accord_HEV_Random_Forest.pickle")
     else:
         raise Exception("Incorrect estimator type")
 
