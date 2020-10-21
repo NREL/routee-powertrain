3912c01d90ad91a4a8d02d0151cff995dd4925ce
nreinick <nicholas.reinicke@nrel.gov>
2020-10-21 15:00:18 -0600

    update functional tests

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,37 +1,86 @@
-import pandas as pd
 import logging as log
 
-from powertrain.estimators import RandomForest, ExplicitBin, LinearRegression
-from powertrain.core.model import Model
+import pandas as pd
+
 from powertrain.core.features import Feature, FeaturePack
+from powertrain.core.model import Model
+from powertrain.estimators import RandomForest, ExplicitBin, LinearRegression
 from powertrain.utils.fs import root
+from tests.mock_resources import mock_route
 
-FILEPATH = root() / "tests" / "routee-powertrain-test-data" / "links_fastsim_2014mazda3.csv"
+log.basicConfig(level=log.INFO)
 
-VEH_NAME = "FUNC TEST - 2014 Mazda 3"
+data_path = root() / "tests" / "routee-powertrain-test-data" / "links_fastsim_2014mazda3.csv"
 
-df = pd.read_csv(FILEPATH, index_col=False)
+out_path = root() / "tests" / "tmp"
+out_path.mkdir(exist_ok=True)
+
+veh_name = "FUNC TEST - 2014 Mazda 3"
+
+df = pd.read_csv(data_path, index_col=False)
 df['grade'] = df.grade * 100.0
-df['speed_mph'] = df['gpsspeed']
 
 features = (
-    Feature('speed_mph', units='mph'),
+    Feature('gpsspeed', units='mph'),
     Feature('grade', units='decimal')
 )
 distance = Feature('miles', units='mi')
 energy = Feature('gge', units='gallons')
 feature_pack = FeaturePack(features, distance, energy)
 
-train_df = df[['miles', 'speed_mph', 'grade', energy.name]].dropna()
-
+train_df = df[['miles', 'gpsspeed', 'grade', energy.name]].dropna()
 train_df = train_df[train_df.miles > 0]
 
 ln_e = LinearRegression(feature_pack=feature_pack)
 rf_e = RandomForest(feature_pack=feature_pack)
 eb_e = ExplicitBin(feature_pack=feature_pack)
 
-for e in (ln_e, rf_e, eb_e):
-    log.info(f"training estimator {e}..")
-    m = Model(e, veh_desc=VEH_NAME)
-    m.train(train_df)
+if __name__ == "__main__":
 
+    for e in (ln_e, rf_e, eb_e):
+        log.info(f"training estimator {e}..")
+        m = Model(e, veh_desc=veh_name)
+        m.train(train_df)
+
+        # test out prediction
+        log.info(f"predicting {m} over test route..")
+        r = m.predict(mock_route())
+
+        energy = round(r.sum(), 2)
+        log.info(f"predicted {energy} gge over test route..")
+
+        # test out writing and reading json
+        log.info(f"writing {m} to json..")
+        json_outfile = out_path.joinpath("model.json")
+        m.to_json(json_outfile)
+
+        log.info(f"reading {json_outfile}..")
+        new_m = Model.from_json(json_outfile)
+
+        log.info(f"predicting {new_m} over test route..")
+        r = new_m.predict(mock_route())
+
+        energy = round(r.sum(), 2)
+        log.info(f"predicted {energy} gge over test route..")
+
+        log.info("removing json file..")
+        json_outfile.unlink()
+
+        # test out writing and reading pickle
+        log.info(f"writing {m} to pickle..")
+        pickle_outfile = out_path.joinpath("model.pickle")
+        m.to_pickle(pickle_outfile)
+
+        log.info(f"reading {pickle_outfile}..")
+        new_m = Model.from_pickle(pickle_outfile)
+
+        log.info(f"predicting {new_m} over test route..")
+        r = new_m.predict(mock_route())
+
+        energy = round(r.sum(), 2)
+        log.info(f"predicted {energy} gge over test route..")
+
+        log.info("removing pickle file..")
+        pickle_outfile.unlink()
+
+    out_path.rmdir()
