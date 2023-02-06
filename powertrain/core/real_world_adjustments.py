b55e471a490091d17775c370ea11d3a66e2d64bd
Nicholas Reinicke <Nicholas.Reinicke@nrel.gov>
2023-02-06 11:48:57 -0700

    correct ICE real world adjustment factor

--- powertrain/core/real_world_adjustments.py
+++ powertrain/core/real_world_adjustments.py
@@ -3,7 +3,7 @@ from powertrain.core.powertrain_type import PowertrainType
 # real world adjustment factors used to generally correct for environmental variables (like temperature)
 ADJUSTMENT_FACTORS = {
     PowertrainType.UNDEFINED: 1,
-    PowertrainType.ICE: 1.116,
+    PowertrainType.ICE: 1.166,
     PowertrainType.HEV: 1.1252,
     PowertrainType.BEV: 1.3958,
 }
