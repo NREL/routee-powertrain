63996565c692af940ed212d5080e9b2b1e711568
Nicholas Reinicke <Nicholas.Reinicke@nrel.gov>
2023-09-13 15:54:59 -0600

    add to/from file methods to estimator; update default models

--- nrel/routee/powertrain/estimators/estimator.py
+++ nrel/routee/powertrain/estimators/estimator.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+from pathlib import Path
 
 import pandas as pd
 
@@ -8,6 +9,19 @@ from nrel.routee.powertrain.core.metadata import Metadata
 
 
 class Estimator(ABC):
+    @classmethod
+    @abstractmethod
+    def from_file(cls, in_dict: dict) -> Estimator:
+        """
+        Load an estimator from a file
+        """
+
+    @abstractmethod
+    def to_file(self, filepath: str | Path):
+        """
+        Save an estimator to a file
+        """
+
     @classmethod
     @abstractmethod
     def from_dict(cls, in_dict: dict) -> Estimator:
@@ -26,4 +40,3 @@ class Estimator(ABC):
         """
         Predict absolute energy consumption for each link
         """
-        pass
